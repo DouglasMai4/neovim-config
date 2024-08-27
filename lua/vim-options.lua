340573bd613b7cc5d0a896c266e87d7bfb5459b7
Douglas Maia <sea.dev.prisma@gmail.com>
2024-08-27 11:51:16 -0300

    feat: Create keymap to see debbug

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,6 +11,9 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Debug keymaps
+vim.keymap.set('n', '<leader>cd', ':lua vim.diagnostic.open_float()<CR>')
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 vim.wo.relativenumber = true
